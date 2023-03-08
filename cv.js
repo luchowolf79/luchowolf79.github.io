5c3a3e735203c3ef751a831caadda63ecb7a27db
luchowolf79 <127265893+luchowolf79@users.noreply.github.com>
2023-03-07 22:06:07 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,18 @@
+
+
+
+document.getElementById('myButton').addEventListener('click', function() {
+    console.log("Hola mundo desde EventListener");
+    document.getElementById("piepag2").innerHTML = "2006-2010 Tech ltd   -   Moontech 2012 2023   -   2011 2023 Weblofi";
+        });
+       
+document.getElementById('myButton2').addEventListener('click', function() {
+    console.log("Hola mundo desde EventListener");
+    document.getElementById("piepag2").innerHTML = "Curso de Informática avanzada - Instituto Campbel 2008 - Postgrado en Informática de la Universidad de A. Coruña  2011";
+        });
+        
+document.getElementById('myButton3').addEventListener('click', function() {
+    console.log("Hola mundo desde EventListener");
+    document.getElementById("piepag2").innerHTML = "david.curtis@example.com  -  7/7/1982  -  4428 Poplar North  -   (857) 506-5263";
+        
+        });
